7d1808218873364ab895b601e621e2a6f09136e0
Aritra Das <adas@vsoftconsulting.com>
2018-11-19 17:36:06 +0530

    .NET core project created

---
+++ IMDBReviewAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMDBReviewAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
